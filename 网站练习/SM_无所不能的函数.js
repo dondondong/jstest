553c9dc536b3c7d31485c00b33da16ff5b9d2b30
Don <>
2023-07-11 20:44:34 +0800

    no message

---
+++ 网站练习/SM_无所不能的函数.js
@@ -0,0 +1,14 @@
+// // 函数给变量赋值
+// function fine(){
+//     console.log('111')
+// }
+
+// // var a = fine;
+// // a()
+
+
+// 对象属性 -> 数据 -> 方法 -> 函数
+// var person = {}
+// person.hello = fine
+// person.hello()
+
