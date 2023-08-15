7e492fa2a5dbedcdef0c9a3dbd9a029fb219895d
Don <>
2023-08-15 10:14:45 +0800

    no message

--- 网站练习/SM_深入对象与类型.js
+++ 网站练习/SM_深入对象与类型.js
@@ -1,62 +1,3 @@
-// 检测对象内是否存在某属性
-// var obj = {};
-// console.log('p' in obj);
-
-
-// 扩展运算符
-// var obj = {
-//     name: 'what'
-// }
-// var obj2 = {
-//     ...obj
-// }
-
-// console.log(obj)
-
-// 转换为数字
-// parseInt('123s.133') // 123 整数
-// parseFloat('123.22.22') // 123.22 一个小数点
-
-// 三元运算符
-// 条件表达式 ? expression1 : expression2
-
-// 一元运算符
-// +true
-// -true
-// +'s'
-// -'123'
-
-// 字符串加号
-// +一个''，可以数字转换为字符串
-// 123 + '' // '123'
-
-// 遍历（迭代）
-// for in
-// for in 只跟对象配合使用
-
-// 遍历对象
-// var person = {
-//     name: '11',
-//     age: '22'
-// }
-
-// for(var key in person){
-//     console.log(key, person[key])
-// }
-
-// 遍历并赋值
-// for(var key in person){
-//     person[key]=66;
-//     console.log(key, person[key])
-// }
-
-// for(var key in person){
-//     if(key === 'age'){
-//         person[key] = '66';
-//     }
-//     console.log(key, person[key])
-// }
-
 
 // -----------------------
 
@@ -251,3 +192,12 @@
 //     }
 // }
 
+// ---------------------------------
+
+// Object 的方法
+// Object.is()方法：判断两个对象是不是同一个值
+// Object.keys()
+// Object.entries()
+// Object.create(null) // 创建一个没有任何原型的对象
+// Object.freeze() // 不能改属性
+// 
