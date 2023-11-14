ad84c32a06c547391c2dfddedf4fd7301237f7f6
dondondong <dondondong@icloud.com>
2023-11-14 21:14:41 +0800

    no message

--- 网站练习/11.HTML变身DOM/SM_DOM.js
+++ 网站练习/11.HTML变身DOM/SM_DOM.js
@@ -1,31 +1,37 @@
-const { Script } = require("vm");
 
-const obj = {
-    tagName: 'html',
-    attributes: [{ lang: 'en' }],
-    childrenTags: [
-        {
-            tagName: 'head',
-            childrenTags: [{
-                tagName: 'title',
-                text: 'DOM练习'
-            }]
-        },
-        {
-            tagName: 'body',
-            childrenTags: [
-                { tagName: 'div' },
-                { tagName: 'div', attributes: [{ id: 'tag' }], text: 'test' },
-                {
-                    tagName: 'ul', childrenTags: [
-                        { tagName: 'li', text: '1' },
-                        { tagName: 'li', text: '2' },
-                        { tagName: 'li', text: '3' }
-                    ]
-                },
-                { tagName: 'img' },
-                { tagName: 'script' },
-            ]
-        }
-    ]
-}
+// HTML的数据转换成对象来表现
+// const obj = {
+//     tagName: 'html',
+//     attributes: [{ lang: 'en' }],
+//     childrenTags: [
+//         {
+//             tagName: 'head',
+//             childrenTags: [{
+//                 tagName: 'title',
+//                 text: 'DOM练习'
+//             }]
+//         },
+//         {
+//             tagName: 'body',
+//             childrenTags: [
+//                 { tagName: 'div' },
+//                 { tagName: 'div', attributes: [{ id: 'tag' }], text: 'test' },
+//                 {
+//                     tagName: 'ul', childrenTags: [
+//                         { tagName: 'li', text: '1' },
+//                         { tagName: 'li', text: '2' },
+//                         { tagName: 'li', text: '3' }
+//                     ]
+//                 },
+//                 { tagName: 'img' },
+//                 { tagName: 'script' },
+//             ]
+//         }
+//     ]
+// }
+
+
+
+
+
+
